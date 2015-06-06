96c888034c777a36af1bf08e957ba8082ba56f2d
Kyle Zhou <kylezhou2002@hotmail.com>
2015-06-05 22:30:39 -0700

    google maps works now

--- app/src/androidTest/java/com/kylemsguy/fishyfishes/ApplicationTest.java
+++ app/src/androidTest/java/com/kylemsguy/fishyfishes/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.kylemsguy.safefishing;
+package com.kylemsguy.fishyfishes;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/kylemsguy/fishyfishes/MainActivity.java
+++ app/src/main/java/com/kylemsguy/fishyfishes/MainActivity.java
@@ -1,4 +1,4 @@
-package com.kylemsguy.safefishing;
+package com.kylemsguy.fishyfishes;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
